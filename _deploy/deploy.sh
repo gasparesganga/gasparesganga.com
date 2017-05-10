99dfb8e8cbf6836100ecdadcc168815a5b945be4
Gaspare Sganga <mail@gasparesganga.com>
2017-05-10 11:12:57 +0200

    .

--- _deploy/deploy.sh
+++ _deploy/deploy.sh
@@ -3,11 +3,11 @@ set -e
 
 if [ $TRAVIS_BRANCH == 'production' ]; then
     export JEKYLL_ENV=production
-    export DEPLOY_PATH=www/
+    export DEPLOY_PATH=www
 else
     export JEKYLL_ENV=development
-    export DEPLOY_PATH=test/
+    export DEPLOY_PATH=test
 fi
 
 bundle exec jekyll build
-lftp -e "mirror -R -e _site/ $DEPLOY_PATH; quit" -u $DEPLOY_USER,$DEPLOY_PASS sftp://$DEPLOY_HOST
+lftp -e "mirror -R -e _site $DEPLOY_PATH; quit" -u $DEPLOY_USER,$DEPLOY_PASS sftp://$DEPLOY_HOST
