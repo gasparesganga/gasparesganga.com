f9f2c7c3d6a9acde497c33f8e19ec07758370493
Gaspare Sganga <contact@gasparesganga.com>
2018-10-08 12:54:05 +0200

    .

--- labs/jquery-loading-overlay/_assets/banner.js
+++ labs/jquery-loading-overlay/_assets/banner.js
@@ -2,7 +2,7 @@ $(function(){
     setTimeout(function(){
         $.MessageBox({
             buttonDone  : "Dismiss",
-            message     : "I know you are here for <b>jQuery LoadingOverlay</b>, but if you like it<br>then I'm sure you will love my <a href='/labs/jquery-message-box/'><b>jQuery MessageBox</b></a> plugin too!"
+            message     : "I know you are here for <b>jQuery LoadingOverlay</b>, but if you like it<br>then I'm sure you will love my <a href='/labs/jquery-message-box/' target='_blank'><b>jQuery MessageBox</b></a> plugin too!"
         });
     }, 1000);
 });
